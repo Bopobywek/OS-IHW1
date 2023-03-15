20d85a0fe83ecd0b0dcc0291fd9f3acb201916d7
Andrey Nechesov <alnechesov@edu.hse.ru>
2023-03-15 20:48:15 +0300

    Добавил программу на 6 баллов

---
+++ 6/constants.h
@@ -0,0 +1,10 @@
+//
+// Created by andrew on 12.03.2023.
+//
+
+#ifndef OS_IHW1__CONSTANTS_H_
+#define OS_IHW1__CONSTANTS_H_
+#define BUFFER_SIZE 5000
+#define INDICATOR_SIZE 10
+
+#endif //OS_IHW1__CONSTANTS_H_
