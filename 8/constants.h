8c2d17889da9f6c6276b877c26e73ec9fff47c40
Andrey Nechesov <alnechesov@edu.hse.ru>
2023-03-18 17:26:59 +0300

    Написал программу на 8 баллов

---
+++ 8/constants.h
@@ -0,0 +1,16 @@
+//
+// Created by andrew on 12.03.2023.
+//
+
+#ifndef OS_IHW1__CONSTANTS_H_
+#define OS_IHW1__CONSTANTS_H_
+#define BUFFER_SIZE 5000
+#define INDICATOR_SIZE 10
+
+#define IT_PIPE "transformer.pipe"
+#define TI_PIPE "transformer-to-io.pipe"
+
+char sem_io_wait[] = "semaphore_wait_io.sem";
+char sem_operations[] = "semaphore_operations.sem";
+
+#endif //OS_IHW1__CONSTANTS_H_
